2a74c0f84e719c9e68120b63def94fa32ec4a538
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-11-22 22:23:46 +0300

    profile posts

--- frond-end/src/components/home/index.js
+++ frond-end/src/components/home/index.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const BASE_URL = "http://localhost:5000";
 
 const Home = () => {
+  let navigate = useNavigate();
   const [posts, setPost] = useState([]);
+  const [maxx, setMaxx] = useState();
+  const [moreLikesPostt, setMoreLikesPostt] = useState("");
 
   useEffect(() => {
     getAllPosts();
@@ -13,11 +17,52 @@ const Home = () => {
   const getAllPosts = async () => {
     const posts = await axios.get(`${BASE_URL}/posts/hash?hashtags=coffe`);
 
-    console.log(posts.data);
-    // setPost(posts.data);
+    posts.data.map((ele) => {
+      countLike(ele);
+    });
   };
 
-  return <div>winner in this week in #coffe</div>;
+  let max = 0;
+
+  let moreLikesPost;
+  const countLike = async (ele) => {
+    const likesCount = await axios.get(
+      `${BASE_URL}/likes/count?onPost=${ele._id}`
+    );
+    if (likesCount.data > max) {
+      max = likesCount.data;
+      moreLikesPost = ele._id;
+      setMaxx(max);
+      setMoreLikesPostt(moreLikesPost);
+      setPost(ele);
+    }
+
+    console.log(` max ${max} the post id ${moreLikesPost}`);
+    console.log(
+      ` post with id : ${ele._id}   have likes count  ${likesCount.data}`
+    );
+  };
+
+  const goInside = (id) => {
+    navigate(`/posts/${id}`);
+  };
+  return (
+    <div>
+      {posts ? (
+        <>
+          winner post in this week #coffe
+          <img
+            src={posts.img}
+            onClick={() => {
+              goInside(posts._id);
+            }}
+          />
+        </>
+      ) : (
+        <h1>loading ...</h1>
+      )}
+    </div>
+  );
 };
 
 export default Home;
--- frond-end/src/components/profile/index.js
+++ frond-end/src/components/profile/index.js
@@ -5,7 +5,8 @@ const BASE_URL = "http://localhost:5000";
 
 const Profile = () => {
   const [users, setusers] = useState([]);
-  const [userProfile, setUserProfile] = useState([]);
+  const [userProfile, setUserProfile] = useState();
+  const [userPostss, setUserPostss] = useState([]);
 
   useEffect(() => {
     getAllUsers();
@@ -14,28 +15,48 @@ const Profile = () => {
   const getAllUsers = async () => {
     const allusers = await axios.get(`${BASE_URL}/users/`);
     setusers(allusers.data);
-    // console.log(allusers.data);
-    // let userid = JSON.parse(localStorage.getItem("userId"))
-    // let profile = users.find((ele)=>ele._id == userid)
-    // setUserProfile(profile);
-    // console.log(profile);
+    let userid = JSON.parse(localStorage.getItem("userId"));
+    setUserProfile(allusers.data.find((ele) => ele._id == userid));
+
+    const userPosts = await axios.get(
+      `${BASE_URL}/posts/userPost?postedBy=${userid}`
+    );
+    console.log(userPosts.data);
+    setUserPostss(userPosts.data);
   };
 
-  let userid = JSON.parse(localStorage.getItem("userId"));
-  let profile = users.find((ele) => ele._id == userid);
-  // console.log(profile);
   return (
-    <div>
-      {profile ? (
-        <>
-          <img src={profile.img} />
-          <h3> {profile.username} </h3>
-          <p> {profile.Bio} </p>
-        </>
-      ) : (
-        <h1>loading ...</h1>
-      )}
-    </div>
+    <>
+      <div>
+        {userProfile ? (
+          <>
+            <img src={userProfile.img} />
+            <h3> {userProfile.username} </h3>
+            <p> {userProfile.Bio} </p>
+          </>
+        ) : (
+          <h1>loading ...</h1>
+        )}
+      </div>
+
+      <div>
+        {userPostss ? (
+          <>
+            <h2>Posts </h2>
+
+            {userPostss.length ? (
+              userPostss.map((ele) => {
+                return <img src={ele.img} />;
+              })
+            ) : (
+              <p>no posted yet </p>
+            )}
+          </>
+        ) : (
+          <h1>loading ...</h1>
+        )}
+      </div>
+    </>
   );
 };
 export default Profile;
