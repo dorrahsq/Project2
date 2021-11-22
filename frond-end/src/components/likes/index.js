c6ebc3cb750a0539322a688c34342e4187651f9e
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-11-22 14:01:58 +0300

    fav

--- frond-end/src/components/likes/index.js
+++ frond-end/src/components/likes/index.js
@@ -1,7 +1,47 @@
-import React from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+
+const BASE_URL = "http://localhost:5000";
 
 const Likes = () => {
-  return <div>Likes</div>;
+  let navigate = useNavigate();
+
+  const [userLikes, setUserLikes] = useState();
+
+  useEffect(() => {
+    getAllLikes();
+  }, []);
+
+  const getAllLikes = async () => {
+    const likes = await axios.get(
+      `${BASE_URL}/likes/userLikes?by=${JSON.parse(
+        localStorage.getItem("userId")
+      )}`
+    );
+    setUserLikes(likes.data);
+  };
+
+  const goInside = (id) => {
+    // navigate(`/posts/${id}`);
+  };
+
+  return (
+    <div>
+      {userLikes ? (
+        <>
+          {userLikes.map((ele) => {
+            console.log(ele.onPost.img);
+            return <img src={ele.onPost.img} />;
+          })}
+        </>
+      ) : (
+        <h1>loading ...</h1>
+      )}
+    </div>
+  );
 };
 
 export default Likes;
